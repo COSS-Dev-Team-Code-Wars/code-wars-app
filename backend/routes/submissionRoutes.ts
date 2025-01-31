a7fde71779dfb67a178b6367949560f989b24197
FarrelBeso <vabeso@up.edu.ph>
2025-02-01 02:48:14 +0800

    feat: added getTeamSubmissions function, added such function in routes

--- backend/routes/submissionRoutes.ts
+++ backend/routes/submissionRoutes.ts
@@ -1,13 +1,22 @@
-import { Router } from 'express';
-import { uploadSubmission, downloadSubmission, checkSubmission, viewSubmissionsTP, getAllSubmissions, getLastSubmissionByTeamOfProblem } from '../controllers/submissionController';
+import { Router } from "express";
+import {
+  uploadSubmission,
+  downloadSubmission,
+  checkSubmission,
+  viewSubmissionsTP,
+  getAllSubmissions,
+  getLastSubmissionByTeamOfProblem,
+  getTeamSubmissions,
+} from "../controllers/submissionController";
 
 const router = Router();
 
-router.post('/uploadsubmission', uploadSubmission);
-router.post('/downloadsubmission', downloadSubmission);
-router.post('/checksubmission', checkSubmission);
-router.post('/viewsubmissions', viewSubmissionsTP);
-router.get('/getallsubmissions', getAllSubmissions);
-router.post('/getlastsubmissionbyteam', getLastSubmissionByTeamOfProblem);
+router.post("/uploadsubmission", uploadSubmission);
+router.post("/downloadsubmission", downloadSubmission);
+router.post("/checksubmission", checkSubmission);
+router.post("/viewsubmissions", viewSubmissionsTP);
+router.get("/getallsubmissions", getAllSubmissions);
+router.post("/getlastsubmissionbyteam", getLastSubmissionByTeamOfProblem);
+router.get("/getteamsubmissions", getTeamSubmissions);
 
 export default router;
