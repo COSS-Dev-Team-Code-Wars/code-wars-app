9c0f6cf749c697204d30bb4bea51745d3d0606e3
Justine Guiao <justinepaulguiao@gmail.com>
2025-03-04 17:59:53 +0800

    Created staging branch

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+//@ Pol Myaw was here
+//@ Test commit for staging
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
