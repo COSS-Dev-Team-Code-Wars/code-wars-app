066ff2c5aee98638d27eb2cfbfc8e34ceb485db2
jvcerezo <jvcerezo@up.edu.ph>
2025-03-05 20:00:09 +0800

    OMAGHAAAAD

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import { get } from "lodash";
 
-const baseURL = get(process.env, "REACT_APP_SERVER_URL", "http://localhost:5000");
+// const baseURL = get(process.env, "REACT_APP_SERVER_URL", "http://localhost:5000");
+const baseURL = process.env.REACT_APP_SERVER_URL;
 
 export { baseURL };
