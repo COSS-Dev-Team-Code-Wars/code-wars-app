27881f372bb69d46dc1df5320dbab06a83924fb0
Justine Guiao <justinepaulguiao@gmail.com>
2025-03-12 07:37:41 +0800

    Remove secure on socket connection

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -6,4 +6,5 @@ import { get } from "lodash";
 export const socketClient = io(get(process.env, "REACT_APP_SOCKET_URL", "http://localhost:8000"), {
   transports: ['polling', 'websocket'],
   forceNode: true,
+  secure: false
 });
